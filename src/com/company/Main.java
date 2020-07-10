6f31f34ba7b25772930bab7f25edf605b774539d
unknown <ferlmick857@gmail.com>
2020-07-10 11:26:44 +0200

    poprawka

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        for (int a = 1; a <= 10; a++)
+        {
+            System.out.println();
+            for (int c = 1; c <= 10; c++)
+            {
+                System.out.print(a * c + " ");
+            }
+        }
+    }
+}
